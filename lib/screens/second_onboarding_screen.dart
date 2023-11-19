ccb3b480c5b3cdf89530fd4fa074b8cb931f3ce7
ZayatsDmitry <108630172+ZayatsDmitry@users.noreply.github.com>
2023-11-19 22:26:40 +0300

    start (#1)

--- lib/screens/second_onboarding_screen.dart
+++ lib/screens/second_onboarding_screen.dart
@@ -11,6 +11,7 @@ class _SecondOnBoardingScreenState extends State<SecondOnBoardingScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Colors.white,
       body: Center(
         child: Text("24:33"),
       ),
